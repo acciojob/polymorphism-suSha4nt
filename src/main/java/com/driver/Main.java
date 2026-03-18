2561d8812bf3dbaac4f36ef079fd816a431c8f05
suSha4nt <heysusanta@gmail.com>
2026-03-18 22:15:56 +0530

    polymorphism completed 3rd

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,28 +1,31 @@
 package com.driver;
 
 public class Main {
-        class Product {
+
+    static class Product {
+
         public int product(int x, int y) {
             return x * y;
         }
 
-        public int product(int x, int y,int z) {
+        public int product(int x, int y, int z) {
             return x * y * z;
         }
+
         public double product(double x, double y) {
             return x * y;
         }
-
     }
+
     public static void main(String[] args) {
-          Main obj = new Main();
-        Main.Product p = obj.new Product();
-        int result =  p.product(10, 20);
+        Product p = new Product();
+
+        int result = p.product(10, 20);
         int result2 = p.product(10, 20, 30);
         double result3 = p.product(10.0, 20.1);
+
         System.out.println(result);
         System.out.println(result2);
         System.out.println(result3);
-
     }
 }
